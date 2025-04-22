a920025ee777266f9b8733cd0e30bc415c465d4c
zalypievka <113243598+Xayles@users.noreply.github.com>
2025-04-22 22:55:54 +0300

    Добавьте файлы проекта.

---
+++ Lab04/MusicArtist.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    class MusicArtist
+    {
+        public string StageName;
+        public string FoundersName;
+        public string Country;
+        public string Genre;
+        public int YearFormed;
+        public int DebutYear;
+        public bool IsActive;
+        public bool SupportUkraine;
+
+        public MusicArtist(string stageName, string foundersName, string country, string genre, int yearFormed, int debutYear, bool isActive, bool supportUkraine)
+        {
+            StageName = stageName;
+            FoundersName = foundersName;
+            Country = country;
+            Genre = genre;
+            YearFormed = yearFormed;
+            DebutYear = debutYear;
+            IsActive = isActive;
+            SupportUkraine = supportUkraine;
+        }
+        
+        public int GetCareerYears()
+        {
+            return DateTime.Now.Year - DebutYear;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            Console.WriteLine("\nІнформація про виконавця:");
+            Console.WriteLine($"Сценічне ім'я: {StageName}");
+            Console.WriteLine($"Ім'я засновника: {FoundersName}");
+            Console.WriteLine($"Країна: {Country}");
+            Console.WriteLine($"Музичний жанр: {Genre}");
+            Console.WriteLine($"Рік заснування: {YearFormed}");
+            Console.WriteLine($"Рік дебюту: {DebutYear}");
+            Console.WriteLine($"Чи активний виконавець: {(IsActive ? "Так" : "Ні")}");
+            Console.WriteLine($"Чи підтримує автор Україну: {(SupportUkraine ? "Так" : "Ні")}");
+
+            int careerYears = GetCareerYears();
+            Console.WriteLine($"Кар'єра виконавця {StageName} становить {careerYears} років.");
+        }
+    }
+}
---
+++ Lab04/Program.cs
@@ -0,0 +1,43 @@
+﻿using Lab04;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        Console.InputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Console.Write("Сценічне ім'я виконавця: ");
+        string StageName = Console.ReadLine();
+
+        Console.Write("Ім'я засновника: ");
+        string FoundersName = Console.ReadLine();
+
+        Console.Write("Країна: ");
+        string Country = Console.ReadLine();
+
+        Console.Write("Музичний жанр: ");
+        string Genre = Console.ReadLine();
+
+        Console.Write("Рік заснування: ");
+        int YearFormed = int.Parse(Console.ReadLine());
+
+        Console.Write("Рік дебюту: ");
+        int DebutYear = int.Parse(Console.ReadLine());
+
+        Console.Write("Чи активний виконавець? (y - так / n - ні): ");
+        bool IsActive = Console.ReadLine()?.Trim().ToLower() == "y";
+
+        Console.Write("Чи підтримує автор Україну? (y - так / n - ні): ");
+        bool SupportUkraine = Console.ReadLine()?.Trim().ToLower() == "y";
+
+        MusicArtist musicArtist = new MusicArtist(StageName, FoundersName, Country, Genre, YearFormed, DebutYear, IsActive, SupportUkraine);
+
+        musicArtist.DisplayInfo();
+
+        Console.ReadKey();
+    }
+}
